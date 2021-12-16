599af9c346b8ecd71bdb483d1d7113f619d8151d
tamsHallz <tamshalliday21@gmail.com>
2021-12-16 14:37:58 +0100

    body updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-
 class RecipeHome extends StatefulWidget {
-
   final String title;
 
-  
-  const RecipeHome({ Key? key, required this.title }) : super(key: key);
+  const RecipeHome({Key? key, required this.title}) : super(key: key);
 
   @override
   _RecipeHomeState createState() => _RecipeHomeState();
@@ -17,11 +13,14 @@ class _RecipeHomeState extends State<RecipeHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
-
-      // body: SafeArea(child: Container()),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text('Our App will display here'),
+        ),
+      ),
     );
   }
-} 
+}
